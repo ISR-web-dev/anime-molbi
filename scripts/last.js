e0aacc7b82d3ac76e2340cca3a1c26776dbbca07
ISR-web-dev <137366576+ISR-web-dev@users.noreply.github.com>
2023-11-11 12:55:57 -0500

    Updated

--- scripts/last.js
+++ scripts/last.js
@@ -20,7 +20,16 @@ document.querySelector('.wrong1').addEventListener('click', () => {
     document.querySelector('.wrong1').setAttribute('style', 'background-color: red;');
     document.querySelector('.correct').setAttribute('style', 'background-color: green;');
 
-    document.querySelector('.result').innerHTML = 'Wrong Answer! Completed';
+    document.querySelector('.result').innerHTML = 'Fill The form by clicking the Button below and get your certificate Now!';
+    document.querySelector('.next-q').innerHTML = 'Get Certificate';
+    document.querySelector('.next-link').setAttribute(`href`, `https://forms.gle/aSPAoEJCv7F2RZ2d9`);
+
+    setTimeout(function() {
+        document.querySelector('.main-img').setAttribute('src', 'images/Conragulations.jpg');
+        document.querySelector('.answers').innerHTML = 'Congratulations! Your Are Now A Anime Molbi. You can get Certified as an Anime Molbi By Completing the steps Given Below.<br> Get You Certificate Now!';
+        document.querySelector('.answers').classList.add('last');
+    }, 500);
+    
 
 
 });
@@ -28,7 +37,15 @@ document.querySelector('.wrong2').addEventListener('click', () => {
     document.querySelector('.wrong2').setAttribute('style', 'background-color: red;');
     document.querySelector('.correct').setAttribute('style', 'background-color: green;');
 
-    document.querySelector('.result').innerHTML = 'Wrong Answer! Completed';
+    document.querySelector('.result').innerHTML = 'Fill The form by clicking the Button below and get your certificate Now!';
+    document.querySelector('.next-q').innerHTML = 'Get Certificate';
+    document.querySelector('.next-link').setAttribute(`href`, `https://forms.gle/aSPAoEJCv7F2RZ2d9`);
+
+    setTimeout(function() {
+        document.querySelector('.main-img').setAttribute('src', 'images/Conragulations.jpg');
+        document.querySelector('.answers').innerHTML = 'Congratulations! Your Are Now A Anime Molbi. You can get Certified as an Anime Molbi By Completing the steps Given Below.<br> Get You Certificate Now!';
+        document.querySelector('.answers').classList.add('last');
+    }, 500);
 
 
 
@@ -37,7 +54,16 @@ document.querySelector('.wrong3').addEventListener('click', () => {
     document.querySelector('.wrong3').setAttribute('style', 'background-color: red;');
     document.querySelector('.correct').setAttribute('style', 'background-color: green;');
 
-    document.querySelector('.result').innerHTML = 'Wrong Answer! Completed';
+    document.querySelector('.result').innerHTML = 'Fill The form by clicking the Button below and get your certificate Now!';
+    document.querySelector('.next-q').innerHTML = 'Get Certificate';
+    document.querySelector('.next-link').setAttribute(`href`, `https://forms.gle/aSPAoEJCv7F2RZ2d9`);
+
+
+    setTimeout(function() {
+        document.querySelector('.main-img').setAttribute('src', 'images/Conragulations.jpg');
+        document.querySelector('.answers').innerHTML = 'Congratulations! Your Are Now A Anime Molbi. You can get Certified as an Anime Molbi By Completing the steps Given Below.<br> Get You Certificate Now!';
+        document.querySelector('.answers').classList.add('last');
+    }, 500);
 
 
 
@@ -45,7 +71,16 @@ document.querySelector('.wrong3').addEventListener('click', () => {
 
 document.querySelector('.correct').addEventListener('click', () => {
     document.querySelector('.result').innerHTML = 'Right Answer! Completed';
-
     document.querySelector('.correct').setAttribute('style', 'background-color: green;');
+
+    document.querySelector('.result').innerHTML = 'Fill The form by clicking the Button below and get your certificate Now!';
+    document.querySelector('.next-q').innerHTML = 'Get Certificate';
+    document.querySelector('.next-link').setAttribute(`href`, `https://forms.gle/aSPAoEJCv7F2RZ2d9`);
+
+    setTimeout(function() {
+        document.querySelector('.main-img').setAttribute('src', 'images/Conragulations.jpg');
+        document.querySelector('.answers').innerHTML = 'Congratulations! Your Are Now A Anime Molbi. You can get Certified as an Anime Molbi By Completing the steps Given Below.<br> Get You Certificate Now!';
+        document.querySelector('.answers').classList.add('last');
+    }, 500);
 
 });
